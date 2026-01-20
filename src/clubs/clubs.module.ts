c7a4ee49c0f6e6825489d76785813a63590f0f14
kimgoid8108 <kimgoid8108@gmail.com>
2026-01-20 16:40:54 +0900

    백엔드 수정

--- src/clubs/clubs.module.ts
+++ src/clubs/clubs.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
-
 import { TypeOrmModule } from '@nestjs/typeorm';
-
 import { ClubsService } from './clubs.service';
 import { ClubsController } from './clubs.controller';
 import { Club } from './entities/club.entity';
-import { ClubMembersModule } from '../club-members/club-members.module';  // ✅ 추가
+import { ClubMembersModule } from '../club-members/club-members.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Club]),
-    ClubMembersModule,  // ✅ 추가 (ClubMembersService를 사용하기 위해)
+    ClubMembersModule, // ClubMembersService를 사용하기 위해
+    UsersModule, // UsersService를 사용하기 위해
   ],
   controllers: [ClubsController],
   providers: [ClubsService],
